656f8e056ac4fbd495f95d57ac627f81e6c6a10c
ApazaVictor <167140778+ApazaVictor@users.noreply.github.com>
2024-10-17 10:34:16 -0500

    .

--- src/com/victor/pe/cpresentacion/JPPersonaDocumento.java
+++ src/com/victor/pe/cpresentacion/JPPersonaDocumento.java
@@ -110,7 +110,7 @@ public class JPPersonaDocumento extends javax.swing.JPanel {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 562, Short.MAX_VALUE)
         );
     }// </editor-fold>//GEN-END:initComponents
 
